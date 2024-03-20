5c866a6fc331887fb8b9a3864c9b343a9bfd7475
Cao Thiện Thiên Tân <alxarrow123@gmail.com>
2024-03-20 19:49:36 +0700

    Modified MainLayout.jsx and DocumentApproval/index.jsx

--- src/layouts/main/MainLayout.jsx
+++ src/layouts/main/MainLayout.jsx
@@ -1,40 +1,34 @@
 import React from 'react';
-import Header from '../../Components/Header';
-import Footer from '../../Components/Footer';
+import Header from '../../components/Header/index';
+import Footer from '../../components/Footer/index';
 
 import { Outlet } from 'react-router-dom';
-import Sidebar from '../../Components/Sidebar';
+import Sidebar from '../../components/Sidebar/index';
 import { Col, Row } from 'antd';
 
-
 const MainLayout = () => {
-    return (
-        <>
+  return (
+    <>
+      {/* <div style={{ height: '100%' }}> */}
+      <div style={{ height: '100vh' }}>
+        <Row span={24}>
+          <Header />
+        </Row>
+        <Row style={{ height: '100%' }} span={24}>
+          <Col span={3}>
+            <Sidebar />
+          </Col>
+          <Col span={21}>
+            <Outlet />
+          </Col>
+        </Row>
 
-
-            {/* <div style={{ height: '100%' }}> */}
-            <div style={{ height: '100vh' }}>
-                <Row span={24}>
-                    <Header />
-                </Row>
-                <Row style={{ height: '100%' }}
-
-                    span={24}>
-                    <Col span={3}>
-                        <Sidebar />
-                    </Col>
-                    <Col span={21}>
-                        <Outlet />
-                    </Col>
-                </Row>
-
-                <Row span={24}>
-                    <Footer />
-                </Row>
-            </div>
-            {/* </div> */}
-        </>
-
-    )
-}
+        <Row span={24}>
+          <Footer />
+        </Row>
+      </div>
+      {/* </div> */}
+    </>
+  );
+};
 export default MainLayout;
